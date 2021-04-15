ff173d5a1a8c1baf99ef02caee07fc76d9f866bc
dextter1913 <dextter1913@gmail.com>
2021-04-15 16:59:09 -0500

    Actualizacion

--- conexiones/ConsultaFacturas.php
+++ conexiones/ConsultaFacturas.php
@@ -1,5 +1,6 @@
-<?php 
-        $registros = mysqli_query($conexion, "SELECT nomProd, categoria, cantidad, totalV, nombreCl, apellidoCl, Nfactura, fechaFactura FROM venta 
+<?php   
+require_once '../clases/Conexion.php';
+        $registros = mysqli_query($conexion->EstablecerConexion(), "SELECT nomProd, categoria, cantidad, totalV, nombreCl, apellidoCl, Nfactura, fechaFactura FROM venta 
         INNER JOIN productos ON productos.idProd = venta.producto 
         INNER JOIN cliente ON cliente.idCl = venta.cliente 
         INNER JOIN facturaVentas ON facturaVentas.Nfactura = venta.Factura
