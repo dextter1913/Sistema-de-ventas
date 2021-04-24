e8c72b1e520d2b9995329bb62de8d70936a09870
dextter1913 <dextter1913@gmail.com>
2021-04-24 11:10:10 -0500

    ACtualizacion

--- clases/IngresarProductos.php
+++ clases/IngresarProductos.php
@@ -3,7 +3,7 @@ require_once 'Productos.php';
 require_once 'Conexion.php';
 class IngresarProductos extends Productos
 {
-    function __construct($_nombreProducto, $_precio, $_precioVenta, $_categoria, $_usuario, $_descripcionP)//recibiendo los datos de los productos enviados en losparametros del objetos por metodo post
+    function __construct($_nombreProducto, $_precio, $_precioVenta, $_categoria, $_usuario, $_descripcionP) //recibiendo los datos de los productos enviados en losparametros del objetos por metodo post
     {
         $this->_nombreProducto = $_nombreProducto;
         $this->_precio = $_precio;
@@ -11,15 +11,19 @@ class IngresarProductos extends Productos
         $this->_categoria = $_categoria;
         $this->_descripcionP = $_descripcionP;
         $this->_usuario = $_usuario;
-        
     }
 
     public function InsertarProducto()
     {
+        $_nombreProducto = $this->_nombreProducto;
+        $_precio = $this->_precio;
+        $_precioVenta =  $this->_precioVenta;
+        $_categoria =  $this->_categoria;
+        $_descripcionP =  $this->_descripcionP;
+        $_usuario =  $this->_usuario;
         $conexion = new Conexion();
-        $conexion->EstablecerConexion()->query("INSERT INTO productos(nomProd, precioProd, precioVenta, categoria, descripcionProd, usuario) 
-        VALUES('$this->_nombreProducto','$this->_precio','$this->_precioVenta','$this->_categoria','$this->_descripcionP','$this->_usuario')");
-        $conexion->__destruct();// destruyendo la conexino
+        $conexion->EstablecerConexion()->query("INSERT INTO productos(nomProd, precioProd, precioVenta, nomCategoria, descripcionProd, user) 
+        VALUES('$_nombreProducto','$_precio','$_precioVenta','$_categoria','$_descripcionP','$_usuario')");
+        $conexion->__destruct(); // destruyendo la conexino
     }
-    
 }
